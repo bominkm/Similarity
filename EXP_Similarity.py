a78576c0df52b36d383199ff7f1085398d21de7d
JeongHyun <pjh09890989@gmail.com>
2021-07-15 01:13:32 +0900

    feat:code update

--- EXP_Similarity.py
+++ EXP_Similarity.py
@@ -6,93 +6,90 @@ import time
 
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
-
-from transformers import ElectraModel, ElectraTokenizer
-
-#from transformers import AutoTokenizer, AutoModel
 from sentence_transformers import SentenceTransformer, util
+
 
 
 def read_sum_data(path):
     sum_database = pd.read_csv(path)
-    return sum_database.loc[:,'kobart_sum']
+    sum_database=sum_database[sum_database.kobart_sum.notna()]
+    return sum_database.kobart_sum.values.tolist()
 
 def vectorization(DATABASE,NEWS,type):
     database =  DATABASE
     news = NEWS
-    corpus=[x for x in database]
 
     if type =='tfidf':
-        corpus.append(news)
+        database.append(news)
         vectorizer = TfidfVectorizer()
-        X = vectorizer.fit_transform(corpus)
+        X = vectorizer.fit_transform(database)
         
     if type =='bert':
-        # model_path = './KoSentenceBERT_SKTBERT/output/training_sts'
-        # embedder = SentenceTransformer(model_path)
-        # query = NEWS
+        model_path = 'paraphrase-multilingual-mpnet-base-v2'
+        embedder = SentenceTransformer(model_path)
+        query = NEWS
 
-        # corpus_embeddings = embedder.encode(corpus, convert_to_tensor=True)
-        # query_embedding = embedder.encode(query, convert_to_tensor=True)
-        # print(corpus_embeddings)
-        # print(query_embedding)
-        # X = corpus_embeddings + query_embedding
-        corpus.append(news)
+        corpus_embeddings = torch.load('./Sum_Database/law_encoder.pt')
+        query_embedding = embedder.encode(query, convert_to_tensor=True)
+        X = (corpus_embeddings,query_embedding)
+    
+        # corpus.append(news)
 
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        model = ElectraModel.from_pretrained("monologg/koelectra-small-v3-discriminator")
-        tokenizer = ElectraTokenizer.from_pretrained("monologg/koelectra-small-v3-discriminator")
+        # model = ElectraModel.from_pretrained("monologg/koelectra-small-v3-discriminator")
+        # tokenizer = ElectraTokenizer.from_pretrained("monologg/koelectra-small-v3-discriminator")
         
-        model = model.to(device)
+        # model = model.to(device)
         
-        tokens = {'input_ids': [], 'attention_mask': []}
-        MAX_LENGTH = 256
-        for sentence in corpus:
-            # encode each sentence and append to dictionary
-            ts = tokenizer.tokenize(sentence)
-            input_ids = tokenizer.convert_tokens_to_ids(ts)
+        # tokens = {'input_ids': [], 'attention_mask': []}
+        # MAX_LENGTH = 256
+        # for sentence in corpus:
+        #     # encode each sentence and append to dictionary
+        #     ts = tokenizer.tokenize(sentence)
+        #     input_ids = tokenizer.convert_tokens_to_ids(ts)
 
-            if len(input_ids) >= MAX_LENGTH:
-                input_ids = input_ids[:MAX_LENGTH]
-                attention_mask = [1] * MAX_LENGTH
-            else:
-                n_to_pad = MAX_LENGTH - len(input_ids)
-                attention_mask = ([1] * len(input_ids)) + ([0]* n_to_pad)
-                input_ids = input_ids + ([0] * n_to_pad)
+        #     if len(input_ids) >= MAX_LENGTH:
+        #         input_ids = input_ids[:MAX_LENGTH]
+        #         attention_mask = [1] * MAX_LENGTH
+        #     else:
+        #         n_to_pad = MAX_LENGTH - len(input_ids)
+        #         attention_mask = ([1] * len(input_ids)) + ([0]* n_to_pad)
+        #         input_ids = input_ids + ([0] * n_to_pad)
         
-            input_ids= torch.as_tensor(input_ids)
-            attention_mask = torch.as_tensor(attention_mask)
+        #     input_ids= torch.as_tensor(input_ids)
+        #     attention_mask = torch.as_tensor(attention_mask)
 
-            tokens['input_ids'].append(input_ids)
-            tokens['attention_mask'].append(attention_mask)
-        # reformat list of tensors into single tensor
-        tokens['input_ids'] = torch.stack(tokens['input_ids'])
-        tokens['attention_mask'] = torch.stack(tokens['attention_mask'])
+        #     tokens['input_ids'].append(input_ids)
+        #     tokens['attention_mask'].append(attention_mask)
+        # # reformat list of tensors into single tensor
+        # tokens['input_ids'] = torch.stack(tokens['input_ids'])
+        # tokens['attention_mask'] = torch.stack(tokens['attention_mask'])
 
-        outputs = model(**tokens)
+        # outputs = model(**tokens)
 
-        embeddings = outputs[0]
-        attention_mask = tokens['attention_mask']
-        mask = attention_mask.unsqueeze(-1).expand(embeddings.size()).float()
-        masked_embeddings = embeddings * mask
-        summed = torch.sum(masked_embeddings, 1)
-        summed_mask = torch.clamp(mask.sum(1), min=1e-9)
-        mean_pooled = summed / summed_mask
-        X = mean_pooled.detach().numpy()
+        # embeddings = outputs[0]
+        # attention_mask = tokens['attention_mask']
+        # mask = attention_mask.unsqueeze(-1).expand(embeddings.size()).float()
+        # masked_embeddings = embeddings * mask
+        # summed = torch.sum(masked_embeddings, 1)
+        # summed_mask = torch.clamp(mask.sum(1), min=1e-9)
+        # mean_pooled = summed / summed_mask
+        # X = mean_pooled.detach().numpy()
 
     return X
 
 
 def Cosine_similarity(X,DATABASE,NEWS,type,top_k):
-    news = X[-1]
-    laws = X[:-1]
-    database=DATABASE
     if type == 'tfidf':
+        news = X[-1]
+        laws = X[:-1]
         cos_scores = cosine_similarity(news,laws)[0]
     
     if type =='bert':
-        cos_scores = util.pytorch_cos_sim(news, laws)[0]
+        news = X[1]
+        laws = X[0]
+        cos_scores = util.pytorch_cos_sim(news,laws)[0]
         cos_scores = cos_scores.cpu()
     
     top_results = np.argpartition(-cos_scores, range(top_k))[0:top_k]
@@ -101,14 +98,13 @@ def Cosine_similarity(X,DATABASE,NEWS,type,top_k):
     print("Query:", NEWS)
     print(f"\nTop {top_k} most similar sentences in corpus:")
 
+    final=[]
     for idx in top_results[0:top_k]:
-        print(database[idx].strip(), "(Score: %.4f)" % (cos_scores[idx]))
-
-    result = pd.DataFrame({'corpus':database,
-                            'similarity':cos_scores})
-    final = result.sort_values('similarity').iloc[:top_k,0]
+        print(DATABASE[idx].strip(), "(Score: %.4f)" % (cos_scores[idx]))
+        final.append(DATABASE[idx].strip())
 
     return final
+
 
 def make_simtext(law_path,new_path,type,top_k):
     DATABASE = read_sum_data(law_path)
@@ -125,7 +121,7 @@ if __name__ == '__main__':
     #start = time.time()  # 시작 시간 저장
     parser = argparse.ArgumentParser()
     # Directory paths
-    parser.add_argument('--law-dir', default='./Sum_Database/summary_gpu.csv')
+    parser.add_argument('--law-dir', default='./Sum_Database/0706_KoBart512.csv')
     parser.add_argument('--news-dir', default='./Sum_Database/news.txt')
     parser.add_argument('--emb-type', required=True, choices=['tfidf','fasttext','bert'])
     parser.add_argument('--top-k',default = 3)
@@ -134,7 +130,3 @@ if __name__ == '__main__':
     make_simtext(args.law_dir,args.news_dir,args.emb_type,args.top_k)
          
     #print("time :", time.time() - start)
-
-
-
-
